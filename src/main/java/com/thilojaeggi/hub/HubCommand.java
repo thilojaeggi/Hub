cc92e4c9ebfaf6b9adf2c34d032528348848417a
Thilo Jaeggi <thilojaeggi@frooze.ch>
2020-09-16 00:07:32 +0200

    Updates

--- src/main/java/com/thilojaeggi/hub/HubCommand.java
+++ src/main/java/com/thilojaeggi/hub/HubCommand.java
@@ -4,20 +4,38 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
 
 public class HubCommand extends Command {
+    Configuration configuration;
     public HubCommand() {
-        super("hub");
+        super("hub", "hub.use", "lobby");
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        Plugin plugin = Main.plugin;
+        File file = new File(plugin.getDataFolder(), "config.yml");
+        try {
+            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         ProxiedPlayer player = (ProxiedPlayer) sender;
-        if (!player.getServer().toString().equals("lobby")){
-            ServerInfo target = ProxyServer.getInstance().getServerInfo("lobby");
+        if (!player.getServer().toString().equals(configuration.getString("hub"))){
+            ServerInfo target = ProxyServer.getInstance().getServerInfo(configuration.getString("hub"));
             player.connect(target);
         } else {
             player.sendMessage("Du bist bereits auf dem Lobby Server");
--- src/main/java/com/thilojaeggi/hub/Main.java
+++ src/main/java/com/thilojaeggi/hub/Main.java
@@ -1,18 +1,105 @@
 package com.thilojaeggi.hub;
 
-import net.md_5.bungee.api.plugin.Command;
+import com.google.common.io.ByteStreams;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public final class Main extends Plugin {
-
+    public static Plugin plugin;
+    Configuration Config;
     @Override
     public void onEnable() {
         getProxy().getPluginManager().registerCommand(this, new HubCommand());
+        plugin = this;
+        createFolder();
+
+        Timer timer = new Timer();
+        TimerTask myTask = new TimerTask() {
+            @Override
+            public void run() {
+                ProxyServer.getInstance().broadcast("["+ChatColor.AQUA.toString()+"INFO"+ChatColor.WHITE.toString() + "] " +ChatColor.WHITE.toString() + "Binärzahlen sind " + ChatColor.BOLD.toString() + 1 + ChatColor.WHITE.toString() +  " und " + ChatColor.BOLD.toString() +2);
+            }
+        };
+        timer.schedule(myTask, 600000, 600000);
+    }
+    private void createFolder() {
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+
+        File ConfigFile = new File(getDataFolder(), "config.yml");
+
+        if (!ConfigFile.exists()) {
+            try {
+                ConfigFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        loaderConfiguration();
     }
 
+    private void loaderConfiguration() {
+        try {
+            Config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+            if (!Config.contains("hub")){
+                Config.set("hub", "lobby");
+                saveConfig();
+            }
+            if (!Config.contains("alert")){
+                Config.set("alert", "lobby");
+                saveConfig();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * method "getConfig" you will get the configuration files depends on a string
+     * in this case there are two "Config" and "Lang"
+     */
+
+    public Configuration getConfig(String file) {
+
+        if (file.equalsIgnoreCase("Config")) {
+            if (Config == null) {
+                reloadConfig();
+            }
+            return Config;
+        }
+        return null;
+    }
+
+    public void saveConfig() {
+        if ((Config != null)) {
+            try {
+                ConfigurationProvider.getProvider(YamlConfiguration.class).save(Config, new File(getDataFolder(), "config.yml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void reloadConfig() {
+        loaderConfiguration();
+    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
-
+    public Plugin getInstance() {
+        return plugin;
+    }
 }
